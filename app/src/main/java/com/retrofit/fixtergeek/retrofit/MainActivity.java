3ca33587ad79ca6d770c5380641cd49120c06c5c
SaulSaandovalM <sauldevelop@gmail.com>
2017-05-05 14:18:28 -0500

    subiendo

---
+++ app/src/main/java/com/retrofit/fixtergeek/retrofit/MainActivity.java
@@ -0,0 +1,47 @@
+package com.retrofit.fixtergeek.retrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import retrofit2.Call;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.List;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.pagination_list);
+
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+
+        GitHubClient client = retrofit.create(GitHubClient.class);
+        Call<List<GitHubRepo>> call = client.reposForUser("fs-opensource");
+
+        call.enqueue(new Callback<List<GitHubRepo>>() {
+            @Override
+            public void onResponse(retrofit2.Call<List<GitHubRepo>> call, Response<List<GitHubRepo>> response) {
+                List<GitHubRepo> repos = response.body();
+                listView.setAdapter(new GitHubRepoAdapter(MainActivity.this, repos));
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<List<GitHubRepo>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
